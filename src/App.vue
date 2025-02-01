82a9ee17f566adf74605f265275fa6f839fd5bec
kCat <kcat.fun.stu@gmail.com>
2025-02-01 18:48:13 +0900

    一番近い人の顔が認識できた

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue';
+import * as faceapi from 'face-api.js';
+
+// モデルのロード
+onMounted(() => {
+  Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/weights'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/weights'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/weights')
+  ]).then(startVideo);
+
+  // カメラストリームの取得
+  function startVideo() {
+    const video = document.getElementById('video');
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true
+      })
+      .then((stream) => {
+        video.srcObject = stream;
+
+        // videoが再生されるのを待ってから顔認識を開始
+        video.onplaying = () => {
+          detectFace(video);
+        };
+      })
+      .catch((err) => {
+        console.error('Error accessing webcam: ', err);
+      });
+  }
+
+  // 顔認識を実行
+  async function detectFace(video) {
+    console.log('Face detection started');
+    // 顔認識のループ
+    setInterval(async () => {
+      const detections = await faceapi.detectSingleFace(video)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
+      // faceapi.matchDimensions(canvas, video);
+
+      if (detections) {
+        const box = detections.detection._box;
+        const x = box.x;
+        const y = box.y;
+        if(x && y) {
+          console.log(`Face detected at x: ${x}, y: ${y}`);
+        }
+      }
+    }, 50);
+  }
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <video id="video" width="720" height="560" autoplay muted></video>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#video {
+  visibility: hidden;
 }
 </style>
